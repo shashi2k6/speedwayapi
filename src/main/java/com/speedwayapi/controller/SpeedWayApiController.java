2ad9423c0a2cb5b9323e97218d0f8749f18e5d9f
shashi <shashi2k6@gmail.com>
2021-02-08 19:51:04 -0500

    Initial commit for the rest controller

---
+++ src/main/java/com/speedwayapi/controller/SpeedWayApiController.java
@@ -0,0 +1,13 @@
+package com.speedwayapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpeedWayApiController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Success";
+    }
+}
