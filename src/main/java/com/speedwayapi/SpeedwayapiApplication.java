8c3b0fda6543f759a9c4999f888c03a101c6d135
shashi <shashi2k6@gmail.com>
2021-02-08 19:08:41 -0500

    initial commit

---
+++ src/main/java/com/speedwayapi/SpeedwayapiApplication.java
@@ -0,0 +1,13 @@
+package com.speedwayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwayapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speedwayapi/SpeedwayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedwayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedwayapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
