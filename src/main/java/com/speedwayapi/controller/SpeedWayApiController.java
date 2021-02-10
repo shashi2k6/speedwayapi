6850e61d39a87c509715e6f4a26eccca6232956d
shashi <shashi2k6@gmail.com>
2021-02-10 18:38:56 -0500

    Added test for the add race cars.

--- src/main/java/com/speedwayapi/controller/SpeedWayApiController.java
+++ src/main/java/com/speedwayapi/controller/SpeedWayApiController.java
@@ -1,13 +1,18 @@
 package com.speedwayapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("//api/v1/racecars")
 public class SpeedWayApiController {
 
-    @RequestMapping("/home")
-    public String home(){
-        return "Success message";
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCar(){
+
     }
 }
